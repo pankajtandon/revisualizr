a132ffaa4ba5585435564bf24c3db19129de997e
pankajtandon <pankajtandon@gmail.com>
2018-02-07 20:32:11 -0500

    prettied up image display

--- src/components/scenes/SceneDetailPage.js
+++ src/components/scenes/SceneDetailPage.js
@@ -8,7 +8,9 @@ class SceneDetailPage extends Component {
             <div>
                 <h3>Scene Detail</h3>
                 <div>For sceneId: {this.props.params.id}</div>
-                <Img className="container" src="http://i.imgur.com/2zNzP6Q.png" width={500} height={300} mode='fit' />
+                <div className="container">
+                <Img  src="http://i.imgur.com/2zNzP6Q.png" width={500} height={300} mode='fit' />
+                </div>
                 <SceneFormContainer />
             </div>
         )
